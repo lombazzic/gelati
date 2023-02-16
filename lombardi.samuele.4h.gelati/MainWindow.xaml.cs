e83088173d493a730e3c034c84de61b46eb82daa
lombazzic <samuele.lombardi@studenti.ittsrimini.edu.it>
2023-02-16 10:34:09 +0100

    Ultima versione del codice

---
+++ lombardi.samuele.4h.gelati/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lombardi.samuele._4h.gelati
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Gelati gg = new Gelati("db.csv");
+                dgDati.ItemsSource = gg;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ lombardi.samuele.4h.gelati/models/Gelato.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lombardi.samuele._4h.gelati
+{
+    public class Gelato
+    {
+        public string Tipo { get; set; }  
+        public string Gusto { get; set; }   
+        
+        public string Size { get; set; }   
+
+        public Gelato() { }
+
+        public Gelato(string riga)
+        {
+            string[] colonne = riga.Split(';');
+            Tipo= colonne[0];   
+            Gusto= colonne[1];   
+            Size= colonne[2];
+
+        }
+
+    }
+
+    public class Gelati:List<Gelato> 
+    {
+        public Gelati() { }
+        public Gelati(string nomefile) 
+        {
+            StreamReader fin = new StreamReader(nomefile);
+            fin.ReadLine();
+                
+            while(!fin.EndOfStream) 
+            {
+                String riga = fin.ReadLine();
+                Gelato g = new Gelato(riga);
+                Add(g);
+            }    
+
+            fin.Close();
+           
+        }
+    }
+}
